480ecbe7007940f499c90bcf74b61738494eace5
Emily Chen <chen3227@purdue.edu>
2019-09-15 10:12:27 -0400

    more variables

--- hello_world_2/lib/main.dart
+++ hello_world_2/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-String sprite = "images/image07.gif";
+String sprite = "images/image07.gif", spriteQuestion = "images/image07.gif";
 String sprite1 = "images/image07.gif", sprite2 = "images/image07.gif", sprite3 = "images/image07.gif",
     sprite4 = "images/image07.gif", sprite5 = "images/image07.gif", sprite6 = "images/image07.gif", spriteBot = "images/image07.gif";
 int score = 0;
