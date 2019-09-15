0193fc83efe9c8e472833f76390f7ebeeca6e11d
henrywu00 <wu1461@purdue.edu>
2019-09-14 21:30:21 -0400

    text field methods

--- hello_world_2/lib/main.dart
+++ hello_world_2/lib/main.dart
@@ -3,21 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: 'Ponderosa'
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'INTO THE VOID'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -57,70 +75,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.grey[850],
       ),
       body: Center(
-        drawer: Drawer(
 
-          child: Container(
-            color: Colors.grey[900],
-            child: Column(
-              children: <Widget>[
-                Container(
-                  child: DrawerHeader(
-
-//                  child: Stack(
-//                    Align(
-//                      alignment: Alignment.bottomCenter,
-//                      child: ListTile(
-//                        title: Text('hello'),
-//                        onTap: () {
-//                          Navigator.pop(context);
-//                        },
-//                      ),
-//                    ),
-//                  ),
-
-
-                    child: Container(
-                      child: Text(
-                        "Level 1",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-
-                        ),
-                      ),
-                      alignment: Alignment.bottomLeft,
-                      padding: EdgeInsets.all(20.0),
-                    ),
-
-                    margin: EdgeInsets.all(0.0),
-                    padding: EdgeInsets.all(0.0),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[500],
-                    ),
-                  ),
-                ),
-
-                ListTile(
-                  title: Text('hello',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -172,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 100,
               child: DecoratedBox(
                 decoration: ShapeDecoration(shape: StadiumBorder(),
-                color: Colors.grey,
+                  color: Colors.grey,
 
                 ),
                 child: OutlineButton(
@@ -209,10 +179,68 @@ class _MyHomePageState extends State<MyHomePage> {
 
         ),
       ),
+      drawer: Drawer(
+
+        child: Container(
+          color: Colors.grey[900],
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: DrawerHeader(
+
+//                  child: Stack(
+//                    Align(
+//                      alignment: Alignment.bottomCenter,
+//                      child: ListTile(
+//                        title: Text('hello'),
+//                        onTap: () {
+//                          Navigator.pop(context);
+//                        },
+//                      ),
+//                    ),
+//                  ),
 
 
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-       // This trailing comma makes auto-formatting nicer for build methods.
+                  child: Container(
+                    child: Text(
+                      "Level 1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+
+                      ),
+                    ),
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.all(20.0),
+                  ),
+
+                  margin: EdgeInsets.all(0.0),
+                  padding: EdgeInsets.all(0.0),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[500],
+                  ),
+                ),
+              ),
+
+              ListTile(
+                title: Text('hello',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+
+
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
